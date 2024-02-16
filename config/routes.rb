7049fb7a20a7c019258b6302e06c0ec7390f0cc4
Yordanos gebreselasie <yordibege@gmail.com>
2024-02-16 12:11:41 -0700

    Implement admin categories feature and modify some style

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,10 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
-    resources :categories, except: [:edit, :update, :show]
+    resources :categories, only: [:index, :new, :create]
   end
+
+ 
   get '/about', to: 'about#index', as: 'about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
